635941aa8aaee9fa91fe6db3a6ec164a8b13a434
Vladimir <smirnovv91@gmail.com>
2018-02-02 17:41:08 +0300

    released functions and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+This program prints to the console duplicate files in the specified folder.
+
+# Quickstart
+
+The program must be run using the console, the required argument is the folder.
+
+How to run:
+```bash
+$ python3 pprint_json.py <folder_path>
+```
+Example of script launch on Linux, Python 3.5:
+```bash
+$ python3 duplicates.py ./
+Same files:
+File name: same_file
+File size: [5] bytes
+File paths:
+./test/folder/same_file.txt
+./test/folder/myfolder/Games/same_file.txt
+
+File name: test_file1.html
+File size: [124] bytes
+File paths:
+./test/test_file1.html
+./test/xxxfolder/test_file1.html
+
+File name: SampleAudio_0.7mb.mp3
+File size: [725240] bytes
+File paths:
+./test/SampleAudio_0.7mb.mp3
+./test/folder/SampleAudio_0.7mb.mp3
+./test/folder/myfolder/SampleAudio_0.7mb.mp3
+./test/folder/myfolder/Games/SampleAudio_0.7mb.mp3
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,65 @@
+import os
+import sys
+
+
+def get_dirs_files_list(path):
+    dirs_files_list = []
+    for current_dir, dirs, files in os.walk(path):
+        dirs_files_list.append([current_dir, dirs, files])
+    return dirs_files_list
+
+
+def get_file_list(dirs_files_list):
+    file_list = []
+    for dir_tuple in  dirs_files_list:
+        for file_name in dir_tuple[2]:
+            file_list.append(dir_tuple[0] + "/" + file_name)  # append full path to file
+    return file_list
+
+
+def compare_2_files(file_path_1, file_path_2):
+    if os.path.basename(file_path_1) == os.path.basename(file_path_2):
+        if os.path.getsize(file_path_1) == os.path.getsize(file_path_2):
+            return True
+    else:
+        return False
+
+
+def get_same_files_dict(file_list):
+    same_files_dict = {}
+    for file_1 in file_list:
+        for file_2 in file_list:
+            if file_1 != file_2:
+                if compare_2_files(file_1, file_2):
+                    if os.path.basename(file_1) in same_files_dict:
+                        if file_1 not in same_files_dict[os.path.basename(file_1)][1]:
+                            same_files_dict[os.path.basename(file_1)][1].append(file_1)
+                    else:
+                        same_files_dict[os.path.basename(file_1)] = [[os.path.getsize(file_1)], [file_1]]
+    return same_files_dict
+
+
+def pretty_print_same_files(same_files_dict):
+    print("Same files: ")
+    for file_name in same_files_dict:
+        print("File name: {}".format(file_name))
+        print("File size: {} bytes".format(same_files_dict[file_name][0]))
+        print("File paths:")
+        for file_path in same_files_dict[file_name][1]:
+            print(file_path)
+        print()
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        file_path = sys.argv[1]
+        if os.path.isdir(file_path):
+            file_list = get_file_list(get_dirs_files_list(file_path))
+            same_files_dict = get_same_files_dict(file_list)
+            pretty_print_same_files(same_files_dict)
+        else:
+            print("Argument is not directory")
+    except IndexError:
+        print("Arguments error")
+    except ValueError:
+        print("The specified file format does not match")
