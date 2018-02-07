e093fd14557b7c4432086b40eee4ddc20f099c0d
Vladimir <smirnovv91@gmail.com>
2018-02-07 12:53:49 +0300

    fixed main func, fixed name values, fixed literals in main func

--- duplicates.py
+++ duplicates.py
@@ -3,45 +3,46 @@ import sys
 import collections
 
 
-def get_file_key_dict(path):
-    file_key_dict = collections.defaultdict(list)
+def get_file_location_dict(path):
+    file_location_dict = collections.defaultdict(list)
     for root, _, filenames in os.walk(path):
         for file_name in filenames:
             file_path = os.path.join(root, file_name)
             file_size = os.path.getsize(file_path)
-            file_key_dict[(file_name, file_size)].append(file_path)
-    return file_key_dict
+            file_location_dict[(file_name, file_size)].append(file_path)
+    return file_location_dict
 
 
-def get_duplicate_files(filename_size_dict):
-    duplicate_dict = filename_size_dict
-    for file_key in list(filename_size_dict):
-        if len(filename_size_dict[file_key]) <= 1:
-            filename_size_dict.pop(file_key)
+def get_duplicate_files(file_location_dict):
+    duplicate_dict = file_location_dict
+    for file_name in list(duplicate_dict):
+        file_locations = duplicate_dict[file_name]
+        if len(file_locations) <= 1:
+            duplicate_dict.pop(file_name)
     return duplicate_dict
 
 
 def pretty_print_same_files(duplicate_dict):
-    if duplicate_dict:
-        print("Same files: ")
-        for file_key in duplicate_dict:
-            print("File name: {}".format(file_key[0]))
-            print("File size: {} bytes".format(file_key[1]))
-            print("File paths:")
-            for file_path in duplicate_dict[file_key]:
-                print(file_path)
-            print()
-    else:
-        print("There are no duplicates in the directory")
+    print("Same files: ")
+    for file_key in duplicate_dict:
+        print("File name: {}".format(file_key[0]))
+        print("File size: {} bytes".format(file_key[1]))
+        print("File paths:")
+        for file_path in duplicate_dict[file_key]:
+            print(file_path)
+        print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         file_path = sys.argv[1]
         if os.path.isdir(file_path):
-            file_key_dict = get_file_key_dict(file_path)
-            duplicates = get_duplicate_files(file_key_dict)
-            pretty_print_same_files(duplicates)
+            file_location_dict = get_file_location_dict(file_path)
+            duplicates = get_duplicate_files(file_location_dict)
+            if duplicates:
+                pretty_print_same_files(duplicates)
+            else:
+                print("There are no duplicates in the directory")
         else:
             print("Argument is not directory")
     except IndexError:
