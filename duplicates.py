54e3031ada55134c02a8216c2871e0db58b5d302
Vladimir <smirnovv91@gmail.com>
2018-02-12 12:13:15 +0300

    changed variables name, added unpacking variables

--- duplicates.py
+++ duplicates.py
@@ -3,20 +3,19 @@ import sys
 import collections
 
 
-def get_file_location_dict(path):
-    file_location_dict = collections.defaultdict(list)
+def get_files_locations_dict(path):
+    files_locations_dict = collections.defaultdict(list)
     for root, _, filenames in os.walk(path):
         for file_name in filenames:
             file_path = os.path.join(root, file_name)
             file_size = os.path.getsize(file_path)
-            file_location_dict[(file_name, file_size)].append(file_path)
-    return file_location_dict
+            files_locations_dict[(file_name, file_size)].append(file_path)
+    return files_locations_dict
 
 
-def get_duplicate_files(file_location_dict):
-    duplicate_dict = file_location_dict
-    for file_name in list(duplicate_dict):
-        file_locations = duplicate_dict[file_name]
+def get_duplicate_files(files_locations_dict):
+    duplicate_dict = files_locations_dict
+    for file_name, file_locations in list(duplicate_dict.items()):
         if len(file_locations) <= 1:
             duplicate_dict.pop(file_name)
     return duplicate_dict
@@ -24,13 +23,12 @@ def get_duplicate_files(file_location_dict):
 
 def pretty_print_same_files(duplicate_dict):
     print("Same files: ")
-    for file in duplicate_dict:
-        file_name = file[0]
-        file_size = file[1]
+    for file_name_tuple, file_locations in duplicate_dict.items():
+        file_name, file_size = file_name_tuple
         print("File name: {}".format(file_name))
         print("File size: {} bytes".format(file_size))
         print("File paths:")
-        for file_path in duplicate_dict[file]:
+        for file_path in file_locations:
             print(file_path)
         print()
 
@@ -42,7 +40,7 @@ if __name__ == "__main__":
         print("Arguments error")
     else:
         if os.path.isdir(file_path):
-            file_location_dict = get_file_location_dict(file_path)
+            file_location_dict = get_files_locations_dict(file_path)
             duplicates = get_duplicate_files(file_location_dict)
             if duplicates:
                 pretty_print_same_files(duplicates)
